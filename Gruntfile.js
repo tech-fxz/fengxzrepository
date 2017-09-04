e4a810635c8c9d3cd987ad0c5587e14f238d093a
fxz <fengxzes@163.com>
2017-09-04 22:49:27 +0800

    jinglecat

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
 
 };
